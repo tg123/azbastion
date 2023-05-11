d74c15bc42c8a16f759ad8e8a1fc54a0664f9029
Boshi LIAN <bolian@microsoft.com>
2023-05-10 17:06:57 -0700

    support flags

--- cmd/bastion-tunnel/main.go
+++ cmd/bastion-tunnel/main.go
@@ -9,6 +9,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/tg123/azbastion/pkg/azbastion"
 	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2/altsrc"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -30,83 +31,117 @@ func main() {
 		keyvaultKeyName string
 	}
 
+	flags := []cli.Flag{
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "subscription",
+			Usage:       "subscription id",
+			EnvVars:     []string{"AZURE_SUBSCRIPTION_ID"},
+			Destination: &config.subscription,
+			// Required:    true,
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "group",
+			Usage:       "bastion resource group",
+			EnvVars:     []string{"AZURE_RESOURCE_GROUP"},
+			Destination: &config.group,
+			// Required:    true,
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "name",
+			Usage:       "bastion name",
+			EnvVars:     []string{"AZURE_BASTION_NAME"},
+			Destination: &config.name,
+			// Required:    true,
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "target-addr",
+			Usage:       "target address",
+			EnvVars:     []string{"TARGET_ADDR"},
+			Destination: &config.targetAddr,
+			// Required:    true,
+		}),
+		altsrc.NewUintFlag(&cli.UintFlag{
+			Name:        "target-port",
+			Usage:       "target port",
+			EnvVars:     []string{"TARGET_PORT"},
+			Destination: &config.targetPort,
+			// Required:    true,
+		}),
+
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "local-addr",
+			Usage:       "local address",
+			EnvVars:     []string{"LOCAL_ADDR"},
+			Destination: &config.localAddr,
+			Value:       "127.0.0.1",
+		}),
+		altsrc.NewUintFlag(&cli.UintFlag{
+			Name:        "local-port",
+			Usage:       "local port",
+			EnvVars:     []string{"LOCAL_PORT"},
+			Destination: &config.localPort,
+			// Required:    true,
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "token",
+			Usage:       "azure access token",
+			EnvVars:     []string{"AZURE_TOKEN"},
+			Destination: &config.token,
+			Required:    false,
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "ssh-keyvault-url",
+			Usage:       "azure keyvault url for ssh private key",
+			EnvVars:     []string{"SSH_KEYVAULT_URL"},
+			Destination: &config.keyvaultUrl,
+			Required:    false,
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "ssh-keyvault-keyname",
+			Usage:       "azure keyvault key name for ssh private key",
+			EnvVars:     []string{"SSH_KEYVAULT_KEY_NAME"},
+			Destination: &config.keyvaultKeyName,
+			Required:    false,
+		}),
+		&cli.StringFlag{
+			Name:  "config",
+			Usage: "config yaml file path",
+			Value: fmt.Sprintf("%v.yaml", strings.TrimSuffix(os.Args[0], ".exe")),
+		},
+	}
+
+	requiredflags := []string{
+		"subscription",
+		"group",
+		"name",
+		"target-addr",
+		"target-port",
+		"local-port",
+	}
+
 	app := &cli.App{
 		Name:  "bastion-tunnel",
 		Usage: "create tcp tunnel via azure bastion",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "subscription",
-				Usage:       "subscription id",
-				EnvVars:     []string{"AZURE_SUBSCRIPTION_ID"},
-				Destination: &config.subscription,
-				Required:    true,
-			},
-			&cli.StringFlag{
-				Name:        "group",
-				Usage:       "bastion resource group",
-				EnvVars:     []string{"AZURE_RESOURCE_GROUP"},
-				Destination: &config.group,
-				Required:    true,
-			},
-			&cli.StringFlag{
-				Name:        "name",
-				Usage:       "bastion name",
-				EnvVars:     []string{"AZURE_BASTION_NAME"},
-				Destination: &config.name,
-				Required:    true,
-			},
-			&cli.StringFlag{
-				Name:        "target-addr",
-				Usage:       "target address",
-				EnvVars:     []string{"TARGET_ADDR"},
-				Destination: &config.targetAddr,
-				Required:    true,
-			},
-			&cli.UintFlag{
-				Name:        "target-port",
-				Usage:       "target port",
-				EnvVars:     []string{"TARGET_PORT"},
-				Destination: &config.targetPort,
-				Required:    true,
-			},
+		Flags: flags,
+		Before: altsrc.InitInputSourceWithContext(flags, func(cCtx *cli.Context) (altsrc.InputSourceContext, error) {
+			if filePath := cCtx.String("config"); filePath != "" {
+				if _, err := os.Stat(filePath); err == nil {
+					log.Printf("loading config from %v", filePath)
+					return altsrc.NewYamlSourceFromFile(filePath)
+				}
+			}
 
-			&cli.StringFlag{
-				Name:        "local-addr",
-				Usage:       "local address",
-				EnvVars:     []string{"LOCAL_ADDR"},
-				Destination: &config.localAddr,
-				Value:       "127.0.0.1",
-			},
-			&cli.UintFlag{
-				Name:        "local-port",
-				Usage:       "local port",
-				EnvVars:     []string{"LOCAL_PORT"},
-				Destination: &config.localPort,
-				Required:    true,
-			},
-			&cli.StringFlag{
-				Name:        "token",
-				Usage:       "azure access token",
-				EnvVars:     []string{"AZURE_TOKEN"},
-				Destination: &config.token,
-				Required:    false,
-			},
-			&cli.StringFlag{
-				Name:        "ssh-keyvault-url",
-				Usage:       "azure keyvault url for ssh private key",
-				EnvVars:     []string{"SSH_KEYVAULT_URL"},
-				Destination: &config.keyvaultUrl,
-				Required:    false,
-			},
-			&cli.StringFlag{
-				Name:        "ssh-keyvault-keyname",
-				Usage:       "azure keyvault key name for ssh private key",
-				EnvVars:     []string{"SSH_KEYVAULT_KEY_NAME"},
-				Destination: &config.keyvaultKeyName,
-				Required:    false,
-			},
-		},
+			return &altsrc.MapInputSource{}, nil
+		}),
 		Action: func(c *cli.Context) error {
+
+			// check required flags
+			for _, f := range requiredflags {
+				if !c.IsSet(f) {
+					return fmt.Errorf("missing required flag: %s", f)
+				}
+			}
+
 			cred, err := createCred(config.token)
 			if err != nil {
 				return err
--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.2.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/keyvault/internal v0.7.1 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v0.9.0 // indirect
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
@@ -32,4 +33,5 @@ require (
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
